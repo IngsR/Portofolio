9e128449a3ae55116f0917b287525eb0d894bb6c
IkhwanRamadhan <ikhwan2ende@gmail.com>
2026-08-26 00:14:39 +0700

    google console

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+document.title = "Ikhwan Ramadhan";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
